1ce293cb2720a74cd77332071c2da19cc52062a3
Daigor <daigortrinh@gmail.com>
2015-08-31 17:03:01 -0700

    implement rooms

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -31,21 +31,53 @@ app.fetch = function(){
 	});
 }
 
-app.realTime = function(){
-
-}
-
 app.display = function(){
 	var name;
 	var text;
 	var div;
+	var roomnames;
+	// filters data for strings and numbers only 
+	var filterFunction = function(data){
+		if (typeof data !== 'string'){
+			return false;
+		}
+		return true;
+	};
+	//maps data by roomnames and only takes the unique of those roomnames, then filters by filter function above
+	roomnames = _.filter(_.uniq(_.map(app.messages, function(obj){return obj['roomname'];})),filterFunction);
 	$('#chats').text('');
+	//all is equal to a jquery div with an id of 'all'
+	var all = $('<div/>').attr('id','all');
+	//targets the node with chat ID and appends the <div id = all>
+	$('#chats').append(all);
+	//targets the node with all ID and appends a header that has all the rooms
+	$('#all').append($('<h3>All Rooms</h3>'));
+	//loops through unique roomname array
+	for (var r = 0; r < roomnames.length; r++){
+		// creates a div
+		div = $('<div/>');
+		// adds an id with the rooms name
+		div.attr("id", roomnames[r]);
+		$('#chats').append(div);
+		h3 = $('<h3/>');
+		h3.text(roomnames[r]);
+		div.append(h3);
+	}
+
 	for(var i = 0; i < app.messages.length; i++){
 		name = app.messages[i].username;
 		text = app.messages[i].text;
+		roomname = app.messages[i].roomname;
+
+		//All rooms
 		div = $('<div/>');
-			div.text(name + ":" + text);
-			$('#chats').append(div);
+		div.text(name + ":" + text);
+		$('#all').append(div);
+
+		//Single room
+		div = $('<div/>');
+		div.text(name + ":" + text);
+		$('#' + roomname).append(div);
 	}
 };
 
